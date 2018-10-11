817e38c158010ed76193f950b8cf3929326f007c
csatchell <satchdev@gmail.com>
2018-10-11 00:19:21 -0400

    Completed login redirects, signup and login forms

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
 
+
 def signup(request):
     if request.method == 'POST':
         if request.POST['password1'] == request.POST['password2']:
@@ -17,16 +18,40 @@ def signup(request):
 
 
 def login(request):
-    if request.method == 'POST':
-        user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+
+    state = "Please log in below..."
+    username = password = ''
+
+    next = ""
+
+    if request.GET:
+        next = request.GET['next']
+
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
         if user is not None:
             auth.login(request, user)
-            return redirect('home')
+            state = "You're successfully logged in!"
+            if next == "":
+                return redirect('home')
+            else:
+                return redirect(next)
         else:
-            return render(request, 'accounts/login.html', {'error': 'username or password is incorrect.'})
-    else:
-        return render(request, 'accounts/login.html')
+            state = "Your username and/or password were incorrect."
 
+    return render(
+        request,
+        'accounts/login.html',
+        {
+        'state': state,
+        'username': username,
+        'next': next,
+        },
+
+    )
 
 def logout(request):
     if request.method == 'POST':
--- products/models.py
+++ products/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=128)
+    body = models.CharField(max_length=200)
+    url = models.CharField(max_length=128)
+    pub_date = models.DateTimeField(default=timezone.now)
+    votes_total = models.PositiveSmallIntegerField(default=1)
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='images/')
+
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+    def summary(self):
+        return self.body[:100]
+
+    def __str__(self):
+        return self.title
